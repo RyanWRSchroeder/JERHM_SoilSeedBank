47e6221e34e23284452e36f37b58e453f27ee1c4
Schroeder <rs199638@umconnect.umt.edu>
2023-02-01 13:50:17 -0700

    Working with cover class data to convert classes to numeric

--- 1_code/1_DataWrangling_AbvVeg_20230201.R
+++ 1_code/1_DataWrangling_AbvVeg_20230201.R
@@ -17,7 +17,7 @@ INT_20.21.22$Treatment = as.factor(INT_20.21.22$Treatment)
 INT_20.21.22$Interspace_Num = as.factor(INT_20.21.22$Interspace_Num)
 INT_20.21.22$Transect_Num = as.factor(INT_20.21.22$Transect_Num)
 INT_20.21.22$Year = as.factor(INT_20.21.22$Year)
-INT_20.21.22$Grazed_Ungrazed = as.factor(INT_20.21.22$Grazed_Ungrazed)
+INT_20.21.22$Grazed_Ungrazed = as.character(INT_20.21.22$Grazed_Ungrazed)
 
 # Drop unneeded variables: Date, Initials_1, Initials_2, Azimuth, Random_1 through Random_4 
 INT_20.21.22 = INT_20.21.22 %>% 
@@ -27,3 +27,24 @@ str(INT_20.21.22)
 # Replace "N/A" values to be na in R
 INT_20.21.22 = INT_20.21.22 %>% 
   mutate(across(where(is.character), ~na_if(., "N/A"))) # replaces "N/A" with true na missing values (as data were not recorded)
+str(INT_20.21.22)
+
+INT_20.21.22$Grazed_Ungrazed = as.factor(INT_20.21.22$Grazed_Ungrazed) # coerce to be a factor
+
+# Replace na values in Annual_Forb_Seelding_count to be 0s since since observed
+INT_20.21.22 = INT_20.21.22 %>% mutate(An_Forb_Seedling_count = ifelse(is.na(An_Forb_Seedling_count), 0, An_Forb_Seedling_count)) # replace na values with 0
+
+#Replace Cover classes (Bailey and Poulton, 1968) in the cover class columns with the mean value of the cover class and coerce to numeric
+
+class_string = c('0'='0', 'T'='0.5', '1'='2.5', '2'='15', '3'='37.5', '4'='62.5', '5'='85', '6'='97.5') # numbers that correspond to each class number
+class_string
+
+INT_20.21.22_CoverPercent = INT_20.21.22
+str(INT_20.21.22_CoverPercent)
+
+INT_20.21.22_CoverPercent['Bare_Class'][INT_20.21.22_CoverPercent['Bare_Class'] == '6'] = 97.5
+
+
+library(stringr)
+# INT_20.21.22$Bare_Class = str_replace_all(INT_20.21.22$Bare_Class, class_string) # replace class values
+
