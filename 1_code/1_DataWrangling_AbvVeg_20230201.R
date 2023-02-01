944ea75aac706e2be212d7d0cce1cd0437c314bd
Schroeder <rs199638@umconnect.umt.edu>
2023-02-01 10:56:37 -0700

    Commit heading out to meeeting

--- 1_code/1_DataWrangling_AbvVeg_20230201.R
+++ 1_code/1_DataWrangling_AbvVeg_20230201.R
@@ -17,5 +17,13 @@ INT_20.21.22$Treatment = as.factor(INT_20.21.22$Treatment)
 INT_20.21.22$Interspace_Num = as.factor(INT_20.21.22$Interspace_Num)
 INT_20.21.22$Transect_Num = as.factor(INT_20.21.22$Transect_Num)
 INT_20.21.22$Year = as.factor(INT_20.21.22$Year)
+INT_20.21.22$Grazed_Ungrazed = as.factor(INT_20.21.22$Grazed_Ungrazed)
 
-# Drop unneeded variables: Date, Initials_1, Initials_2, Azimuth, Random_1 through Random_4, Notes, 
+# Drop unneeded variables: Date, Initials_1, Initials_2, Azimuth, Random_1 through Random_4 
+INT_20.21.22 = INT_20.21.22 %>% 
+  select(-Date, -Initials_1, -Initials_2, -Azimuth, -Random_1, -Random_2, - Random_3, -Random_4)
+str(INT_20.21.22)
+
+# Replace "N/A" values to be na in R
+INT_20.21.22 = INT_20.21.22 %>% 
+  mutate(across(where(is.character), ~na_if(., "N/A"))) # replaces "N/A" with true na missing values (as data were not recorded)
